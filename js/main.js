a8f0a41c535e7e627d38cf10cf3b6cd943bdc59b
Nilo Peligro <nilopeligro27@gmail.com>
2020-02-21 20:48:50 +1000

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  //listen for form submit
+  document.getElementById('myForm').addEventListener('submit',saveBookmark);
+  
+  //save bookmark
+  function saveBookmark(e){
+    
+    //get form value
+    var siteName =document.getElementById('siteName').value;
+    var siteUrl =document.getElementById('siteUrl').value;
+
+    var bookmark = {
+      name: siteName,
+      url: siteUrl
+    }
+    
+    //local storage test
+    localStorage.setItem('test', "Hello World");
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+    //prevent flash submit
+    e.preventDefault();
+  }
