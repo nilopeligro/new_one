e4ba67350abafd8d772618300337d375cf4f7032
Nilo Peligro <nilopeligro27@gmail.com>
2020-02-22 15:04:37 +1000

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -23,11 +23,30 @@ function myFunction() {
       url: siteUrl
     }
     
-    //local storage test
-    localStorage.setItem('test', "Hello World");
-    console.log(localStorage.getItem('test'));
-    localStorage.removeItem('test');
-    console.log(localStorage.getItem('test'));
+      /*
+      //local storage test
+      localStorage.setItem('test', "Hello World");
+      console.log(localStorage.getItem('test'));
+      localStorage.removeItem('test');
+      console.log(localStorage.getItem('test'));
+      */
+
+    //test if bookmarkis null
+    if(localStorage.getItem('bookmarks') === null ) {
+      //init array
+      var bookmarks = [];
+      //add to array
+      bookmarks.push(bookmark);
+      //set to localstorage
+      localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+      //bookmarks from local storage
+      var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+      //add bookmarks to array
+      bookmarks.push(bookmark);
+      //reset back to localstorage
+      localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
     //prevent flash submit
     e.preventDefault();
   }
